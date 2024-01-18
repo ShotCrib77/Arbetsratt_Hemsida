ba7fe76abc56a62a2fbc283256c721d1fb8fa341
Wilmer Säfsten <Wilmer.Safsten@gmail.com>
2024-01-18 15:30:29 +0100

    Co-authored-by: vincentwwww <vincentwwww@users.noreply.github.com>

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+function scrollToTarget(id) {
+  var targetElement = document.getElementById(id);
+
+  console.log(targetElement);
+
+  window.scrollTo({
+      top: targetElement.offsetTop - 96 - 34,
+      behavior: 'smooth'
+  });
+}
